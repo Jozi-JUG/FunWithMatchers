d94329e7ad874fc502b0ffe33e4caf2b5f17931a
Niel de Wet <nieldw+github@gmail.com>
2015-01-29 22:04:23 +0200

    Initial test setup, with constants.

---
+++ src/test/java/nieldw/example/matchers/factory/ContactDetailsDtoFactoryTest.java
@@ -0,0 +1,24 @@
+package nieldw.example.matchers.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContactDetailsDtoFactoryTest {
+
+    private static final long CONTACT_DETAILS_ID = 222L;
+    private static final long PERSON_ID = 333L;
+    private static final String SOME_TELEPHONE_NUMBER = "some telephone number";
+    private static final String SOME_CELLPHONE_NUMBER = "some cellphone number";
+    private static final String SOME_POSTAL_ADDRESS = "some postal address";
+    private static final String SOME_HOME_ADDRESS = "some home address";
+
+    @Test
+    public void testBuildThinContactDetailsDTO() throws Exception {
+        // Set up fixture
+
+        // Exercise SUT
+
+        // Verify behaviour
+    }
+}
---
+++ src/test/java/nieldw/example/matchers/factory/PersonDtoFactoryTest.java
@@ -0,0 +1,20 @@
+package nieldw.example.matchers.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonDtoFactoryTest {
+
+    private static final long PERSON_ID = 111L;
+    private static final String SOME_PERSON_NAME = "some person name";
+
+    @Test
+    public void testBuild() throws Exception {
+        // Set up fixture
+
+        // Exercise SUT
+
+        // Verify behaviour
+    }
+}
