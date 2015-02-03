c91e20d48a4b13408520c81f39e92fae4886089a
Niel de Wet <nieldw+github@gmail.com>
2015-02-03 21:51:40 +0200

    Test implemented with hamcrest matcher.

--- src/test/java/nieldw/example/matchers/factory/ContactDetailsDtoFactoryTest.java
+++ src/test/java/nieldw/example/matchers/factory/ContactDetailsDtoFactoryTest.java
@@ -4,8 +4,10 @@ import nieldw.example.matchers.dto.ContactDetailsDTO;
 import nieldw.example.matchers.entity.ContactDetails;
 import org.junit.Test;
 
+import static nieldw.example.matchers.builder.dto.ContactDetailsDTOBuilder.aContactDetailsDTO;
 import static nieldw.example.matchers.builder.entity.ContactDetailsBuilder.aContactDetails;
-import static org.junit.Assert.assertEquals;
+import static nieldw.example.matchers.matcher.HasContactDetailsDTOBeenCreatedCorrectly.hasSameStateAs;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactDetailsDtoFactoryTest {
 
@@ -28,16 +30,19 @@ public class ContactDetailsDtoFactoryTest {
                 .withPostalAddress(SOME_POSTAL_ADDRESS)
                 .withHomeAddress(SOME_HOME_ADDRESS)
                 .build();
+        ContactDetailsDTO expectedContactDetails = aContactDetailsDTO()
+                .withId(CONTACT_DETAILS_ID)
+                .withPersonId(PERSON_ID)
+                .withTelephoneNumber(SOME_TELEPHONE_NUMBER)
+                .withCellphoneNumber(SOME_CELLPHONE_NUMBER)
+                .withPostalAddress(SOME_POSTAL_ADDRESS)
+                .withHomeAddress(SOME_HOME_ADDRESS)
+                .build();
 
         // Exercise SUT
-        ContactDetailsDTO contactDetailsDTO = contactDetailsDtoFactory.buildThinContactDetailsDTO(contactDetails);
+        ContactDetailsDTO returnedContactDetailsDTO = contactDetailsDtoFactory.buildThinContactDetailsDTO(contactDetails);
 
         // Verify behaviour
-        assertEquals(CONTACT_DETAILS_ID, contactDetailsDTO.getId());
-        assertEquals(PERSON_ID, contactDetailsDTO.getPersonId());
-        assertEquals(SOME_TELEPHONE_NUMBER, contactDetailsDTO.getTelephoneNumber());
-        assertEquals(SOME_CELLPHONE_NUMBER, contactDetailsDTO.getCellphoneNumber());
-        assertEquals(SOME_POSTAL_ADDRESS, contactDetailsDTO.getPostalAddress());
-        assertEquals(SOME_HOME_ADDRESS, contactDetailsDTO.getHomeAddress());
+        assertThat(returnedContactDetailsDTO, hasSameStateAs(expectedContactDetails));
     }
 }
---
+++ src/test/java/nieldw/example/matchers/matcher/HasContactDetailsDTOBeenCreatedCorrectly.java
@@ -0,0 +1,81 @@
+package nieldw.example.matchers.matcher;
+
+import nieldw.example.matchers.dto.ContactDetailsDTO;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.util.Objects;
+
+/**
+ * ContactDetailsDTO matcher.
+ */
+public class HasContactDetailsDTOBeenCreatedCorrectly extends TypeSafeMatcher<ContactDetailsDTO> {
+
+    private ContactDetailsDTO itemToBeComparedTo;
+
+    public HasContactDetailsDTOBeenCreatedCorrectly(ContactDetailsDTO contactDetailsDTOToCompareTo) {
+        this.itemToBeComparedTo = contactDetailsDTOToCompareTo;
+    }
+
+    @Override
+    protected boolean matchesSafely(ContactDetailsDTO item) {
+        return item.getId() == itemToBeComparedTo.getId() &&
+                item.getPersonId() == itemToBeComparedTo.getPersonId() &&
+                item.getCellphoneNumber().equals(itemToBeComparedTo.getCellphoneNumber()) &&
+                item.getTelephoneNumber().equals(itemToBeComparedTo.getTelephoneNumber()) &&
+                item.getHomeAddress().equals(itemToBeComparedTo.getHomeAddress()) &&
+                item.getPostalAddress().equals(itemToBeComparedTo.getPostalAddress());
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("a ContactDetailsDTO with the following state:");
+        description.appendText("\nId: ").appendValue(itemToBeComparedTo.getId());
+        description.appendText("\nPersonId: ").appendValue(itemToBeComparedTo.getPersonId());
+        description.appendText("\nCellphoneNumber: ").appendValue(itemToBeComparedTo.getCellphoneNumber());
+        description.appendText("\nTelephoneNumber: ").appendValue(itemToBeComparedTo.getTelephoneNumber());
+        description.appendText("\nHomeAddress: ").appendValue(itemToBeComparedTo.getHomeAddress());
+        description.appendText("\nPostalAddress: ").appendValue(itemToBeComparedTo.getPostalAddress());
+    }
+
+    @Override
+    protected void describeMismatchSafely(ContactDetailsDTO item, Description mismatchDescription) {
+        mismatchDescription.appendText("all state fields did not match, mismatched fields are:");
+        if (item.getId() != itemToBeComparedTo.getId()) {
+            mismatchDescription.appendText("\n\tId: ")
+                    .appendText("actual = ").appendValue(item.getId())
+                    .appendText(", expected = ").appendValue(itemToBeComparedTo.getId());
+        }
+        if (item.getPersonId() != itemToBeComparedTo.getPersonId()) {
+            mismatchDescription.appendText("\n\tPersonId: ")
+                    .appendText("actual = ").appendValue(item.getPersonId())
+                    .appendText(", expected = ").appendValue(itemToBeComparedTo.getPersonId());
+        }
+        if (!Objects.equals(item.getCellphoneNumber(), itemToBeComparedTo.getCellphoneNumber())) {
+            mismatchDescription.appendText("\nCellphoneNumber: ")
+                    .appendText("actual = ").appendValue(item.getCellphoneNumber())
+                    .appendText(", expected = ").appendValue(itemToBeComparedTo.getCellphoneNumber());
+        }
+        if (!Objects.equals(item.getTelephoneNumber(), itemToBeComparedTo.getTelephoneNumber())) {
+            mismatchDescription.appendText("\nTelephoneNumber: ")
+                    .appendText("actual = ").appendValue(item.getTelephoneNumber())
+                    .appendText(", expected = ").appendValue(itemToBeComparedTo.getTelephoneNumber());
+        }
+        if (!Objects.equals(item.getHomeAddress(), itemToBeComparedTo.getHomeAddress())) {
+            mismatchDescription.appendText("\nHomeAddress: ")
+                    .appendText("actual = ").appendValue(item.getHomeAddress())
+                    .appendText(", expected = ").appendValue(itemToBeComparedTo.getHomeAddress());
+        }
+        if (!Objects.equals(item.getPostalAddress(), itemToBeComparedTo.getPostalAddress())) {
+            mismatchDescription.appendText("\nPostalAddress: ")
+                    .appendText("actual = ").appendValue(item.getPostalAddress())
+                    .appendText(", expected = ").appendValue(itemToBeComparedTo.getPostalAddress());
+        }
+    }
+
+    @Factory
+    public static HasContactDetailsDTOBeenCreatedCorrectly hasSameStateAs(ContactDetailsDTO contactDetailsDTO) {
+        return new HasContactDetailsDTOBeenCreatedCorrectly(contactDetailsDTO);
+    }
+}
